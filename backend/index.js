5fcac32c6479b99d82b6f48015b2ac013ca82b15
Bryan Yan <bryanyan.tx@gmail.com>
2022-09-05 02:23:50 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT = 3030;
+const app = express();
+const todoRoutes = require("./routes/todoRoutes");
+//const connectionOptions = {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false};
+app.use(express.json());
+app.use(cors());
+mongoose.connect("mongodb://localhost/todolist")
+    .then(()=> console.log("connected successfully"))
+    .catch((err)=> console.error(err));
+
+app.use("/todos", todoRoutes);
+
+app.listen(PORT, ()=> {
+    console.log("The server is listening on "+PORT);
+});
